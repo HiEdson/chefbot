89960e4e9f14ef21113e366f85dfb4c51b521e49
Edson <edsoncasimiro264@gmail.com>
2023-02-25 00:51:10 +0300

    add novos compoments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './components/Home.js'
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="" style={{ backgroundColor:'#F6F4F9' }}>
-      <Home/>
+     <Home/>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import chef from '../img/chef.png'
-import './Navbar.css'
-const Navbar =()=>{
+import chef from '../img/chef.png';
 
+const Navbar =()=>{
     return(
-        <nav class="navbar shadow rounded-bottom fixed-top">
+        <nav class="navbar shadow rounded-bottom bg-black fixed-top">
             <div class="container-fluid">
-                <a class="navbar-brand mx-auto" href="#">
-                    <img src={chef} width="50" height="54"  className=''/>
+                <a class="navbar-brand mx-auto " href="#">
+                    <img src={chef} width="50" height="54"/>
                     <b className='text-white'>ChefBot</b>
                 </a>
             </div>
         </nav>
     )
 }
+
 export default Navbar;
